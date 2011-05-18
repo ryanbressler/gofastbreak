a817bc04f58b67563307e8ab274fbcaa29d42327
Ryan Bressler <ryanbressler@gmail.com>
2011-05-17 23:42:46 -0700

    code to output google datatable

--- fastbreakapp/transplantws.go
+++ fastbreakapp/transplantws.go
@@ -30,7 +30,35 @@ package fastbreakapp
 import (
     "fmt"
     "http"
+    "os"
+    "json"
 )
+
+type googleDataTable struct {
+    cols	[]map[string]string
+    rows	[]map[string][]map[string]string
+}
+
+//there must be a better way to do this
+func getGoogleDataTableJson(cols []string,rows [][]string) ([]byte, os.Error){
+	out := googleDataTable{cols: make([]map[string]string,len(cols)),
+							rows: make([]map[string][]map[string]string,len(rows))}
+	
+	for _,col := range cols{
+		out.cols=append(out.cols,map[string]string{"id":col,"type":"string"})
+		}
+	for _,row := range rows{
+		rowout := make([]map[string]string,len(row))
+		for _,val := range row{
+			rowout=append(rowout,map[string]string{"v":val})
+			}
+		out.rows=append(out.rows,map[string][]map[string]string{"c":rowout})
+	}
+	//TODO: return this error?
+	return json.Marshal(out)
+	
+
+	}
 
 func init() {
     http.HandleFunc("/transplantdata", dataserviceHandler)
